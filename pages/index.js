1329237d56bf032114d99aa7c8724db302a3087f
Madan Bajgai <madanbajgai@gmail.com>
2022-07-07 17:58:17 +0545

    solved big issue: moved component folder out of the pages folder

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-import ProductFeeds from "./components/ProductFeed";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeeds from "../components/ProductFeed";
+
 
 export default function Home({ products }) {
-  console.log(products);
   return (
     <div className=" bg-gray-100">
       <Head>
